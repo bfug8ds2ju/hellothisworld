8937107ee0ab147659763e305f432de17ac278d6
bfug8ds2ju <90911881+bfug8ds2ju@users.noreply.github.com>
2025-10-21 03:40:01 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 base64 -d config > config.json
-UUID=${UUID:-'1872b7c8-8421-4464-8fb3-83a33258266d'}
+UUID=${UUID:-'0c0b36bc-3bf7-43a3-83cd-f6d763e2e2ba'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
